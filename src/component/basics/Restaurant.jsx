d8476f0e3549abb21cc1b74ffce5f78e176ed70d
Maryanos <maryando78@gmail.com>
2023-03-08 09:24:24 +0000

    First Push

--- src/component/basics/Restaurant.jsx
+++ src/component/basics/Restaurant.jsx
@@ -6,15 +6,17 @@ import Navbar from "./Navbar"
 function Restaurant()  {
 
   return (
-    <>
+    <div className="wrapper">
       <Navbar  />
       <div className="cards-wrapper">
         {menu.map((i) => (
           <MenuCard image={i.image} name={i.name} category={i.category} price={i.price} description={i.description} key={i.id}/>
         ))}
       </div>
-    </>
+    </div>
   )
 }
 
 export default Restaurant;
+
+
